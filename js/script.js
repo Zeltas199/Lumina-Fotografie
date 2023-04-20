6a129a1165f018b983c0527f5ec4fe51cad13d0c
Alex <alex.zeltas@gmail.com>
2023-04-21 01:47:51 +0200

    Weitere Seiten hinzugefügt

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
+let header = document.querySelector('.header')
+let upButton = document.querySelector('.scroll-down')
 
 menu.onclick = () =>{
     menu.classList.toggle('bx-x');
@@ -13,7 +15,19 @@ const sr = ScrollReveal ({
     reset: true
 });
 
-sr.reveal('.hero-text',{delay:200,origin:'top'});
-sr.reveal('.hero-img',{delay:450,origin:'right'});
+sr.reveal('.home-text',{delay:200,origin:'top'});
+sr.reveal('.home-img',{delay:450,origin:'right'});
 sr.reveal('.icons',{delay:500,origin:'left'});
 sr.reveal('.scroll-down',{delay:500,origin:'right'});
+sr.reveal('.lisa-text',{delay:500,origin:'right'});
+sr.reveal('.lisa-img',{delay:500,origin:'left'});
+
+window.addEventListener("scroll", function() {
+    if (window.pageYOffset > 50) {
+      header.classList.add("active");
+      upButton.classList.remove("inactive");
+    } else {
+      header.classList.remove("active");
+      upButton.classList.add("inactive")
+    }
+  });
